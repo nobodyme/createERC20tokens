ec5b0ef0808b0cc7f0fafa16f6716f4aa4571400
nobodyme <naveenkumarspa@gmail.com>
2019-08-14 03:57:24 +0530

    basic styles added

--- src/components/Description.js
+++ src/components/Description.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import '../styles/components/Description.css';
+import DescriptionIcon from '../images/coins.svg';
 
 function Description() {
-	return <div className="description">Create ERC20 tokens</div>;
+	return (
+		<div className="description">
+			{/* <img
+				src={DescriptionIcon}
+				alt="description icon"
+				height="400px"
+				width="400px"
+			/> */}
+			<div className="description__tag">Create </div>
+			<div className="description__tag">ERC20 tokens</div>
+		</div>
+	);
 }
 
 export default Description;
